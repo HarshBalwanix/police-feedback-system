d822255bc54e38f2f7094afccac0f30b83d61cff
Harsh Balwani <balwaniharsh1001@gmail.com>
2024-01-11 14:47:36 +0530

    Added footer

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,11 +1,14 @@
 "use client";
 import React from "react";
 import Header from "../../Components/Header";
+import Footer from "../../Components/Footer";
+
 const Dashboard = () => {
   return (
     <>
       <Header />
       <div>Dashboard</div>
+      <Footer />
     </>
   );
 };
