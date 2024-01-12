243eed1d93dce06980bbe63722bfd39c7c09f3b4
Harsh Balwani <balwaniharsh1001@gmail.com>
2024-01-12 22:49:30 +0530

    Added hover effect in star rating

--- src/Components/StarRating.jsx
+++ src/Components/StarRating.jsx
@@ -3,7 +3,17 @@ import React, { useState } from "react";
 const StarRating = ({ onChange }) => {
   const [rating, setRating] = useState(0);
 
+  const handleHover = (hoveredRating) => {
+    setRating(hoveredRating);
+  };
+
+  const handleLeave = () => {
+    // Reset the rating when leaving the container
+    setRating(0);
+  };
+
   const handleClick = (selectedRating) => {
+    // Save the rating immediately on click
     setRating(selectedRating);
     onChange(selectedRating);
   };
@@ -16,6 +26,7 @@ const StarRating = ({ onChange }) => {
           className={`text-2xl ${
             star <= rating ? "text-yellow-500" : "text-gray-300"
           } cursor-pointer transition duration-300 ease-in-out hover:text-yellow-500`}
+          onMouseEnter={() => handleHover(star)}
           onClick={() => handleClick(star)}
         >
           &#9733;
